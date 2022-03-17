867bf527ac703d20892321a975d9c25adeae294b
Ace1313 <alex.cadjo@hotmail.com>
2022-03-17 19:47:44 +0100

    added navigation between components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  loadedFeature: string = 'recipe';
+  onNavigate(random: string) {
+    this.loadedFeature = random;
+  }
   title = 'section-2';
 }
